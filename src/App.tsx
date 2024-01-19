9ce90928897c5ccbd26eb2b0fb99334d097c9340
AlexNi245 <alex.plutta@googlemail.com>
2024-01-19 15:08:08 +0100

    dm3-demo app with react-app-reqired

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+// @ts-ignore
+import { DM3 } from "@dm3-org/dm3-messenger-widget";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const props: any = {
+    defaultContact: "contact.dm3.eth",
+    defaultServiceUrl: process.env.REACT_APP_DEFAULT_SERVICE,
+    ethereumProvider: process.env.REACT_APP_MAINNET_PROVIDER_RPC,
+    walletConnectProjectId: process.env.REACT_APP_WALLET_CONNECT_PROJECT_ID,
+    showAlways: true,
+    hideFunction: "attachments", // OPTINAL PARAMETER : 'attachments,edit,delete' or undefined
+    showContacts: true, // true for all contacts / false for default contact
+    theme: undefined, // OPTINAL PARAMETER : undefined/themeColors
+  };
+  return <DM3 {...props} />;
 }
 
 export default App;
